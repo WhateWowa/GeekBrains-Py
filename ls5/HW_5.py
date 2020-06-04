707c575ba6720289c8d15bb925e3f8db6a2af304
Vladimir <Wowa_89@mail.ru>
2020-06-04 19:07:58 +0300

    test.py
    sad

---
+++ ls5/HW_5.py
@@ -0,0 +1,10 @@
+
+# from functools import reduce
+# list = [i for i in range(100, 1001) if i % 2 ==0]
+# print(list)
+# result = reduce((lambda x, y: x * y), list)
+# print(result)
+
+
+# list1 = [i for i in range(20, 240) if i % 20 == 0] + [i for i in range(20, 240) if i % 21 == 0]
+# print(list1)
