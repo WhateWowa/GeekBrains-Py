fd1d1b382f1bed051c76ad387e14ee741ab39cd1
Vladimir <Wowa_89@mail.ru>
2020-06-10 13:00:35 +0300

    Update HW_6.py

--- HW_6.py
+++ HW_6.py
@@ -7,38 +7,42 @@
 # Задачу можно усложнить, реализовав проверку порядка режимов,
 # и при его нарушении выводить соответствующее сообщение и завершать скрипт.
 
-import time
-
-class TrafficLight:
-
-    __color = ['red', 'yellow', 'green']
-
-    def running(self, __color):
-        self.color = __color
-        for i in self.color:
-            if i = 'Red':
-            time.clock(7)
-        continue self.color is 'Yellow':
-            time.clock(2)
-        continue self.color is 'Green':
-            time.clock(10)
-        break:
-
-color = TrafficLight('blue')
-
-#     if "mode" in i:
-#         if i["mode"][0] == "red":
-#             offLights()
-#             G.output(light_r, True)
-#         elif i["mode"][0] == "yellow":
-#             offLights()
-#             G.output(light_y, True)
-#         elif i["mode"][0] == "green":
-#             offLights()
-#             G.output(light_g, True)
-#         elif i["mode"][0] == "off":
-#             offLights()
-
+# from time import sleep
+# from colorama import Fore
+#
+# class TrafficLight:
+#     def __init__(self, color):
+#         self.__color = color
+#         self.running()
+#
+#     def running(self):
+#         while True:
+#             if self.__color == "Red":
+#                 print(Fore.RED + 'Red - 7 sec')
+#                 sleep(7)
+#                 print(Fore.YELLOW + 'Yellow - 2 sec')
+#                 sleep(2)
+#                 print(Fore.GREEN + 'Green - 5 sec')
+#                 sleep(5)
+#                 break
+#             else:
+#                 if self.__color == 'Yellow':
+#                     print(Fore.YELLOW + 'Yellow - 2 sec')
+#                     sleep(2)
+#                     print(Fore.GREEN + 'Green - 5 sec')
+#                     sleep(5)
+#                     break
+#                 else:
+#                     if self.__color == 'Green':
+#                         print(Fore.GREEN + 'Green - 5 sec')
+#                         sleep(5)
+#                         break
+#                     else:
+#                         print(Fore.LIGHTBLUE_EX + 'Wrong Color!')
+#                         break
+#
+# start_color = TrafficLight('Blue')
+# print(Fore.RESET + "Start color: ", start_color._TrafficLight__color)
 
 # 2. Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина).
 # Значения данных атрибутов должны передаваться при создании экземпляра класса. Атрибуты сделать защищенными.
@@ -47,7 +51,19 @@ color = TrafficLight('blue')
 # толщиной в 1 см * числом толщины полотна. Проверить работу метода.
 # Например: 20м * 5000м * 25кг * 5см = 12500 т
 
-
+# class Road:
+#
+#     def __init__(self, length, width):
+#         self._length = length
+#         self._width = width
+#         self.mass = 25
+#         self.depth = 5
+#         self.calculate()
+#
+#     def calculate(self):
+#         print('Масса асфальта:', (self._length * self._width * self.mass * self.depth) / 1000, 'тонн')
+#
+# road = Road(10, 2000)
 
 # 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
 # name, surname, position (должность), income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь,
@@ -57,6 +73,29 @@ color = TrafficLight('blue')
 # Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
 # проверить значения атрибутов, вызвать методы экземпляров).
 
+dict()
+income1 = {"wage": 20000, "bonus": 5000}
+income2 = {"wage": 22000, "bonus": 3000}
+
+class Worker:
+
+    def __init__(self, name, surname, position):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = dict()
+        print('Имя: ', self.name, '\n', 'Фамилия: ', self.surname, '\n', 'Должность: ', self.position, '\n', 'Доход: ', self._income)
+
+class Position(Worker):
+
+    def get_full_name(self):
+        print('ФИО: ', self.name, self.surname)
+
+    def get_full_income(self):
+        print('Доход: ', )
+
+
+pos1 = Position('Ivan', 'Golunov', 'Spec', income)
 
 
 # 4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты:
