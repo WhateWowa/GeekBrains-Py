0a93b936119fd7394392c98ed8dccd950ed60ab5
Vladimir <Wowa_89@mail.ru>
2020-06-10 16:18:49 +0300

    up

--- HW_6.py
+++ HW_6.py
@@ -7,42 +7,42 @@
 # Задачу можно усложнить, реализовав проверку порядка режимов,
 # и при его нарушении выводить соответствующее сообщение и завершать скрипт.
 
-# from time import sleep
-# from colorama import Fore
-#
-# class TrafficLight:
-#     def __init__(self, color):
-#         self.__color = color
-#         self.running()
-#
-#     def running(self):
-#         while True:
-#             if self.__color == "Red":
-#                 print(Fore.RED + 'Red - 7 sec')
-#                 sleep(7)
-#                 print(Fore.YELLOW + 'Yellow - 2 sec')
-#                 sleep(2)
-#                 print(Fore.GREEN + 'Green - 5 sec')
-#                 sleep(5)
-#                 break
-#             else:
-#                 if self.__color == 'Yellow':
-#                     print(Fore.YELLOW + 'Yellow - 2 sec')
-#                     sleep(2)
-#                     print(Fore.GREEN + 'Green - 5 sec')
-#                     sleep(5)
-#                     break
-#                 else:
-#                     if self.__color == 'Green':
-#                         print(Fore.GREEN + 'Green - 5 sec')
-#                         sleep(5)
-#                         break
-#                     else:
-#                         print(Fore.LIGHTBLUE_EX + 'Wrong Color!')
-#                         break
-#
-# start_color = TrafficLight('Blue')
-# print(Fore.RESET + "Start color: ", start_color._TrafficLight__color)
+from time import sleep
+from colorama import Fore
+
+class TrafficLight:
+    def __init__(self, color):
+        self.__color = color
+        self.running()
+
+    def running(self):
+        while True:
+            if self.__color == "Red":
+                print(Fore.RED + 'Red - 7 sec')
+                sleep(7)
+                print(Fore.YELLOW + 'Yellow - 2 sec')
+                sleep(2)
+                print(Fore.GREEN + 'Green - 5 sec')
+                sleep(5)
+                break
+            else:
+                if self.__color == 'Yellow':
+                    print(Fore.YELLOW + 'Yellow - 2 sec')
+                    sleep(2)
+                    print(Fore.GREEN + 'Green - 5 sec')
+                    sleep(5)
+                    break
+                else:
+                    if self.__color == 'Green':
+                        print(Fore.GREEN + 'Green - 5 sec')
+                        sleep(5)
+                        break
+                    else:
+                        print(Fore.LIGHTBLUE_EX + 'Wrong Color!')
+                        break
+
+start_color = TrafficLight('Blue')
+print(Fore.RESET + "Start color: ", start_color._TrafficLight__color)
 
 # 2. Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина).
 # Значения данных атрибутов должны передаваться при создании экземпляра класса. Атрибуты сделать защищенными.
@@ -51,19 +51,19 @@
 # толщиной в 1 см * числом толщины полотна. Проверить работу метода.
 # Например: 20м * 5000м * 25кг * 5см = 12500 т
 
-# class Road:
-#
-#     def __init__(self, length, width):
-#         self._length = length
-#         self._width = width
-#         self.mass = 25
-#         self.depth = 5
-#         self.calculate()
-#
-#     def calculate(self):
-#         print('Масса асфальта:', (self._length * self._width * self.mass * self.depth) / 1000, 'тонн')
-#
-# road = Road(10, 2000)
+class Road:
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.mass = 25
+        self.depth = 5
+        self.calculate()
+
+    def calculate(self):
+        print('Масса асфальта:', (self._length * self._width * self.mass * self.depth) / 1000, 'тонн')
+
+road = Road(10, 2000)
 
 # 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
 # name, surname, position (должность), income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь,
@@ -73,11 +73,10 @@
 # Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
 # проверить значения атрибутов, вызвать методы экземпляров).
 
-
-
-dict()
-income1 = {"wage": 20000, "bonus": 5000}
-income2 = {"wage": 22000, "bonus": 3000}
+dict = {
+    'wage': 20000,
+    'bonus': 5000
+}
 
 class Worker:
 
@@ -85,25 +84,23 @@ class Worker:
         self.name = name
         self.surname = surname
         self.position = position
-        self._income = dict()
-        print('Имя: ', self.name, '\n', 'Фамилия: ', self.surname, '\n', 'Должность: ', self.position, '\n', 'Доход: ', self._income)
+        x = dict['wage']
+        y = dict['bonus']
+        self._income = int(x + y)
+        print('Имя: ', self.name, '\n', 'Фамилия: ', self.surname, '\n', 'Должность: ', self.position, '\n', 'Доход: ',
+              self._income)
 
 class Position(Worker):
 
     def get_full_name(self):
-        print('ФИО: ', self.name, self.surname)
+        print('ФИО:', self.name, self.surname)
 
     def get_full_income(self):
-
-        sum_inc = sum([int(x[x.find(':'):]) for x in splitted_line[1:] if '(' in x])
-        print('Доход: ', inc)
-
-# sum_lessons = sum([int(x[:x.find('(')]) for x in splitted_line[1:] if '(' in x])
+        print('Доход:', self._income)
 
 pos1 = Position('Ivan', 'Golunov', 'Spec')
 pos1.get_full_name()
-# pos1.get_full_income()
-
+pos1.get_full_income()
 
 # 4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты:
 # speed, color, name, is_police (булево). А также методы: go, stop, turn(direction), которые должны сообщать,
@@ -114,10 +111,127 @@ pos1.get_full_name()
 # Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 # Выполните вызов методов и также покажите результат.
 
+class Car:
 
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print(self.name, 'Поехала')
+
+    def stop(self):
+        print(self.name, 'Остановилась')
+
+    def turn(self):
+        print(self.name, 'Поворот внетуда')
+
+    def show_speed(self):
+        print('Скорость:', self.speed)
+
+class TownCar(Car):
+
+    def Town(self):
+        print('TownCar', self.name, ':', self.speed, 'км/ч')
+
+    def show_speed(self):
+        if self.speed > 40:
+            print(self.name, ', Вы превысили скорость!', 'Ваша скорость: ', self.speed)
+        else:
+            print(self.name, 'Я не превышаю')
+
+class SportCar(Car):
+
+    def Sport(self):
+        print(self.name, 'say: Need For Speed"', '\n', 'Моя скорость: ', self.speed)
+
+class WorkCar(Car):
+
+    def Work(self):
+        print('WorkCar', self.name, ':', self.speed, 'км/ч')
+
+    def show_speed(self):
+        if self.speed > 60:
+            print(self.name, ', Вы превысили скорость!', 'Ваша скорость: ', self.speed)
+        else:
+            print(self.name, 'say: Я не превышаю')
+
+class PoliceCar(Car):
+
+    def Police(self):
+        print(self.name, self.is_police, 'say: Поймать всех нарушителей скоростного режима')
+
+workcar1 = WorkCar(70, 'Blue', 'Lada', 'Work')
+workcar1.go()
+workcar1.show_speed()
+workcar1.turn()
+workcar1.stop()
+workcar2 = WorkCar(50, 'White', 'KIA', 'Work')
+workcar2.go()
+workcar2.WorkCar()
+workcar2.show_speed()
+workcar2.stop()
+towncar1 = TownCar(50, 'White', 'KIA', 'Town')
+towncar1.go()
+towncar1.Town()
+towncar1.show_speed()
+towncar1.stop()
+towncar2 = TownCar(30, 'Black', 'Lada', 'Town')
+towncar2.go()
+towncar2.Town()
+towncar2.turn()
+towncar2.show_speed()
+towncar2.turn()
+towncar2.stop()
+policcar = PoliceCar(120, 'Black-White', 'Ford', 'Police')
+policcar.go()
+policcar.Police()
+policcar.show_speed()
+sportcar = SportCar(200, 'Purple', 'Lamborgini', 'Sport')
+sportcar.go()
+sportcar.turn()
+sportcar.Sport()
+sportcar.show_speed()
 
 # 5. Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название)
 # и метод draw (отрисовка). Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка),
 # Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw.
 # Для каждого из классов методы должен выводить уникальное сообщение. Создать экземпляры классов и проверить,
 # что выведет описанный метод для каждого экземпляра.
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(self.title)
+        print('"Запуск отрисовки."')
+
+class Pen(Stationery):
+
+    def draw(self):
+        print(self.title)
+        print('"Запуск отрисовки ручкой"')
+
+class Pencil(Stationery):
+
+    def draw(self):
+        print(self.title)
+        print('"Запуск отрисовки карандашом"')
+
+class Handle(Stationery):
+
+    def draw(self):
+        print(self.title)
+        print('"Запуск отрисовки маркером"')
+
+palka = Stationery('palka')
+palka.draw()
+pen = Pen('Pen')
+pen.draw()
+pencil = Pen('Pencil')
+pencil.draw()
+handle = Pen('Handle')
+handle.draw()
